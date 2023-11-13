476113947f5f91d241b4354a3b7cab0d668e4ec1
Scrobez <jorgeluis-sc@outlook.es>
2023-11-12 19:27:11 -0800

    Se termino la vista de login

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gin/constants/colors.dart';
+import 'package:gin/constants/gaps.dart';
+import 'package:gin/views/widgets/custom_text_form_field.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -14,20 +16,71 @@ class _LoginViewState extends State<LoginView> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: primaryColor,
-        body: Column(
-          children: [
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: Image.asset(
-                'assets/images/gin_logo.png',
-                fit: BoxFit.contain,
-              ),
-            ),
-            TextFormField()
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: _buildBody(),
         ),
       ),
+    );
+  }
+
+  Column _buildBody() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 150,
+          width: 150,
+          child: Image.asset(
+            'assets/images/gin_logo.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+        verticalBigGap,
+        const CustomTextFormField(
+          hintText: 'Usuario',
+        ),
+        verticalBigGap,
+        const CustomTextFormField(
+          hintText: 'Contraseña',
+        ),
+        verticalBigGap,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Checkbox(
+              fillColor: const MaterialStatePropertyAll(Colors.white),
+              value: false,
+              onChanged: (value) {},
+            ),
+            const Text(
+              'Recordar contraseña',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            )
+          ],
+        ),
+        TextButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor: const MaterialStatePropertyAll(Colors.white),
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0.0),
+              ),
+            ),
+          ),
+          child: const Text(
+            'INICIAR SESIÓN',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
+        verticalBigGap,
+      ],
     );
   }
 }
