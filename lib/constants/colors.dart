f883730875453bc62b4f3ad0143cbe4d1a5e3ea0
Scrobez <jorgeluis-sc@outlook.es>
2023-11-13 20:44:11 -0800

    Se creo el dashboard

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const Color primaryBlack = Color(0xFF0F111C);
 const Color lightGray = Color(0xFFC1C2C3);
+const Color blueMarine = Color(0xFF2A628F);
+const Color pink = Color(0xFFCB9CF2);
--- lib/views/home/dashboard/dashboard_view.dart
+++ lib/views/home/dashboard/dashboard_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gin/constants/colors.dart';
+import 'package:gin/constants/gaps.dart';
 import 'package:gin/views/home/dashboard/dashboard_viewmodel.dart';
+import 'package:gin/views/home/widgets/discount_widget.dart';
+import 'package:gin/views/widgets/custom_text_button.dart';
 
 class DashboardView extends StatelessWidget {
   DashboardView({super.key});
@@ -10,13 +13,73 @@ class DashboardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+      child: Column(
+        children: [
+          DiscountWidget(
+            discount: '50% DE\nDESCUENTO',
+            limitedEdition: true,
+            onPressed: () {},
+          ),
+          verticalBigGap,
+          DiscountWidget(
+            discount: '40% DE\nDESCUENTO',
+            limitedEdition: true,
+            onPressed: () {},
+          ),
+          verticalBigGap,
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ProductWidget(
+                productName: 'Licuadora',
+                productPrice: '\$60',
+              ),
+              ProductWidget(
+                productName: 'Camara Fujifilm',
+                productPrice: '\$60',
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProductWidget extends StatelessWidget {
+  const ProductWidget({
+    super.key,
+    required this.productName,
+    required this.productPrice,
+  });
+
+  final String productName;
+  final String productPrice;
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Container(
+          height: 125,
+          width: 130,
           color: primaryBlack,
-          width: 200,
-          height: 100,
-        )
+        ),
+        Text(
+          productName,
+          style: const TextStyle(fontSize: 16),
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            productPrice,
+            style: const TextStyle(fontSize: 14),
+          ),
+        ),
       ],
     );
   }
