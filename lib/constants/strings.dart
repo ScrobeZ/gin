7650cd7a81976261cd22e4ac4f88295274586823
Guillermo-CM <Guillermo.castm@hotmail.com>
2023-11-17 20:22:44 -0800

    Merge conflict resolved

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -3,11 +3,7 @@ const String loginRoute = '/login';
 const String registerRoute = '/register';
 const String homeRoute = '/home';
 const String dashboardRoute = '/dashboard';
-<<<<<<< HEAD
 const String categoryRoute = '/category';
 const String categoryProductsRoute = '/categoryproducts';
-=======
 const String shoppingCartRoute = '/shopping_cart';
-
 const String fakeStoreURL = 'https://fakestoreapi.com';
->>>>>>> f8baa4950669db574da5b45bd5486ed48876571d
