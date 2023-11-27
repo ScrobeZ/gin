6b6bbcf9eedad39677d690079dafff8321683694
Scrobez <jorgeluis-sc@outlook.es>
2023-11-26 22:49:30 -0800

    Ya carga productos del api en el carrito

--- lib/views/home/shopping_cart/shopping_cart_view.dart
+++ lib/views/home/shopping_cart/shopping_cart_view.dart
@@ -17,77 +17,85 @@ class _ShoppingCartViewState extends State<ShoppingCartView> {
     return Container(
       child: !model.isAnyProducts
           ? FutureBuilder(
-              future: model.getProduct(1),
+              future: model.getCart(),
               builder: (context, snapshot) {
-                return snapshot.hasData
-                    ? ListView.builder(
-                        itemCount: 1,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            leading: Image.network(snapshot.data!.image,
-                                fit: BoxFit.fill, height: 50),
-                            title: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                SizedBox(
-                                  width: size.width * 0.4,
-                                  child: Text(
-                                    snapshot.data!.title,
-                                    style: const TextStyle(fontSize: 12),
-                                    maxLines: 3,
-                                  ),
+                if (snapshot.hasData) {
+                  return Center(
+                    child: ListView.builder(
+                      itemCount: model.cart.products.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Image.network(
+                            snapshot.data![index].image,
+                            fit: BoxFit.contain,
+                            height: 50,
+                            width: 50,
+                          ),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              SizedBox(
+                                width: size.width * 0.4,
+                                child: Text(
+                                  snapshot.data![index].title,
+                                  style: const TextStyle(fontSize: 12),
+                                  maxLines: 3,
                                 ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    IconButton(
-                                      style: const ButtonStyle(
-                                        minimumSize: MaterialStatePropertyAll(
-                                            Size(5, 5)),
-                                        backgroundColor:
-                                            MaterialStatePropertyAll(
-                                                Colors.red),
-                                      ),
-                                      padding: const EdgeInsets.all(1),
-                                      onPressed: () {},
-                                      icon: const Icon(
-                                        Icons.remove,
-                                        color: primaryBlack,
-                                        size: 15,
-                                      ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  IconButton(
+                                    style: const ButtonStyle(
+                                      minimumSize:
+                                          MaterialStatePropertyAll(Size(5, 5)),
+                                      backgroundColor:
+                                          MaterialStatePropertyAll(Colors.red),
                                     ),
-                                    Text('${model.productLength}'),
-                                    IconButton(
-                                      style: const ButtonStyle(
-                                        minimumSize: MaterialStatePropertyAll(
-                                            Size(5, 5)),
-                                        backgroundColor:
-                                            MaterialStatePropertyAll(
-                                                Colors.green),
-                                      ),
-                                      padding: const EdgeInsets.all(1),
-                                      onPressed: () {},
-                                      icon: const Icon(
-                                        Icons.add,
-                                        color: primaryBlack,
-                                        size: 15,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                            subtitle: Text(
-                              '\$${snapshot.data!.price}',
-                              style: const TextStyle(fontSize: 10),
-                            ),
-                          );
-                        },
-                      )
-                    : const Center(
-                        child: CircularProgressIndicator(
-                        color: primaryBlack,
-                      ));
+                                    padding: const EdgeInsets.all(1),
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.remove,
+                                      color: primaryBlack,
+                                      size: 15,
+                                    ),
+                                  ),
+                                  Text(
+                                      '${model.cart.products[index].quantity}'),
+                                  IconButton(
+                                    style: const ButtonStyle(
+                                      minimumSize:
+                                          MaterialStatePropertyAll(Size(5, 5)),
+                                      backgroundColor: MaterialStatePropertyAll(
+                                          Colors.green),
+                                    ),
+                                    padding: const EdgeInsets.all(1),
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.add,
+                                      color: primaryBlack,
+                                      size: 15,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          subtitle: Text(
+                            '\$${snapshot.data![index].price}',
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      color: primaryBlack,
+                    ),
+                  );
+                }
               },
             )
           : const Center(
--- lib/views/home/shopping_cart/shopping_cart_view_model.dart
+++ lib/views/home/shopping_cart/shopping_cart_view_model.dart
@@ -1,15 +1,27 @@
+import 'package:gin/models/cart.dart' as shopping_cart;
 import 'package:gin/models/product.dart';
 import 'package:gin/services/fake_store_services.dart';
 
 class ShoppingCartViewModel {
   bool isAnyProducts = false;
   bool isBusy = false;
-
-  int productLength = 1;
+  late shopping_cart.Cart cart;
 
   final FakeStoreService _fakeStoreService = FakeStoreService();
 
-  Future<Product> getProduct(int id) async {
-    return await _fakeStoreService.getProduct(1);
+  Future<List<Product>> getCart() async {
+    cart = await _fakeStoreService.getCart();
+
+    final List<int> ids;
+
+    List<Product> products = [];
+
+    ids = cart.products.map((e) => e.productId).toList();
+
+    for (var i = 0; i < ids.length; i++) {
+      products.add(await _fakeStoreService.getProduct(ids[i]));
+    }
+
+    return products;
   }
 }
